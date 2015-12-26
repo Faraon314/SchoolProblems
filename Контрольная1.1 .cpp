73d57e5f466c1e6c88c3a45d71d604d7d6f1b462
Sergey Kutukov <skutukov96@gmail.com>
2015-12-27 00:41:35 +0300

    Update file

--- Контрольная1.1 .cpp
+++ Контрольная1.1 .cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+/*
+Ввести с клавиатуры стороны треугольника и вычислить угол, 
+противолежащий стороне, которая была введена первой.
+*/
+int main()
+{
+double a,b,c;
+cin>>c>>b>>a;//вводим стороны
 
+cout<<(acos((a*a+b*b-c*c)/(2*a*b)))<<endl;//по теореме косинусов
+
+return EXIT_SUCCESS;
+}
